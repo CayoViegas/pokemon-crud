ef7728881b3ac2a56d0d654203469fe0daf5d4b3
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2025-04-09 00:34:14 -0300

    feat: migrate from SQLite to PostgreSQL with .env config support

--- database/database.go
+++ database/database.go
@@ -1,24 +1,52 @@
 package database
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"pokemon-crud/models"
 
-	"gorm.io/driver/sqlite"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	var err error
-	DB, err = gorm.Open(sqlite.Open("pokemon.db"), &gorm.Config{})
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		getEnv("DB_HOST"),
+		getEnv("DB_USER"),
+		getEnv("DB_PASSWORD"),
+		getEnv("DB_NAME"),
+		getEnv("DB_PORT"),
+	)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
 
 	err = DB.AutoMigrate(&models.Pokemon{})
+
 	if err != nil {
 		log.Fatal("Failed to migrate database:", err)
 	}
+}
+
+func getEnv(key string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+
+	log.Fatalf("Environment variable %s is not set", key)
+
+	return ""
 }
